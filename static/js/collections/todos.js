591c2a15aa4db985335987ee135b49141f6f584f
Kevin Beaty <kevin@simplectic.com>
2014-01-06 20:32:27 -0600

    Remove localStorage persistence

--- static/js/collections/todos.js
+++ static/js/collections/todos.js
@@ -13,9 +13,6 @@ var app = app || {};
 		// Reference to this collection's model.
 		model: app.Todo,
 
-		// Save all of the todo items under the `"todos"` namespace.
-		localStorage: new Backbone.LocalStorage('todos-backbone'),
-
 		// Filter down the list of all todo items that are finished.
 		completed: function () {
 			return this.filter(function (todo) {
--- static/js/views/app-view.js
+++ static/js/views/app-view.js
@@ -39,11 +39,6 @@ var app = app || {};
 			this.listenTo(app.todos, 'change:completed', this.filterOne);
 			this.listenTo(app.todos, 'filter', this.filterAll);
 			this.listenTo(app.todos, 'all', this.render);
-
-			// Suppresses 'add' events with {reset: true} and prevents the app view
-			// from being re-rendered for every model. Only renders when the 'reset'
-			// event is triggered at the end of the fetch.
-			app.todos.fetch({reset: true});
 		},
 
 		// Re-rendering the App just means refreshing the statistics -- the rest
