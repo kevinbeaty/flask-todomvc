c9f4a9e5eb6ff2dda9e5f12ca8a58f20da44aac9
Kevin Beaty <kevin@simplectic.com>
2015-08-03 18:29:55 -0500

    Add Flask-Debug-API

--- server-debug.py
+++ server-debug.py
@@ -1,8 +1,22 @@
 from flask_todomvc.factory import create_app
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_debug_api import DebugAPIExtension
+
 app = create_app()
 app.debug = True
 
-from flask_debugtoolbar import DebugToolbarExtension
+# Debug Extensions
 DebugToolbarExtension(app)
+DebugAPIExtension(app)
+
+# Append Browse API Panel to defaults
+config = app.config
+panels = list(config['DEBUG_TB_PANELS'])
+config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+panels.append('flask_debug_api.BrowseAPIPanel')
+config['DEBUG_TB_PANELS'] = panels
+
+# Change API prefix from /api to /todos
+config['DEBUG_API_PREFIX'] = '/todos'
 
 app.run(port=8000)
