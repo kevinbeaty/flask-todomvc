949fa908e7b212232baa25e2bbdd6df9ebaf71e3
Bence Nagy <nagy@bence.dev>
2023-05-19 11:03:29 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,15 @@
 """ server.py """
+
 from flask_todomvc.factory import create_app
 app = create_app()
 app.run(port=8000)
+
+import psycopg2
+
+conn = psycopg2.connect(
+    "replace this string with connection details, include a password='something' field"
+)
+
+conn = psycopg2.connect("dbname=test user=postgres password='thisisapassword!#@'")
+
+print("ye")
